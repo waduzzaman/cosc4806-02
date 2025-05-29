f04eb192e78bf840249de030b70e9b1e3d126c8f
Md Mahbub  E Waduzzaman <45024227+waduzzaman@users.noreply.github.com>
2025-05-29 15:43:51 +0000

    Add database configuration with environment variable for password

--- database.php
+++ database.php
@@ -1,4 +1,9 @@
 <?php
+define('DB_HOST',         'dvmmh.h.filess.io');
+define('DB_USER',         'cosc4806_badlyearth'); 
+define('DB_PASS',         $_ENV['DB_PASS']);
+define('DB_DATABASE',     'cosc4806_badlyearth');
+define('DB_PORT',         '61000');
 
 
 ?>
