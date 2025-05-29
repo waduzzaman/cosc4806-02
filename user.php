7991fbdb4a184bc6a6d47954ab2636d351a36f0e
Md Mahbub  E Waduzzaman <45024227+waduzzaman@users.noreply.github.com>
2025-05-29 16:40:37 +0000

    Add method to get all users

--- user.php
+++ user.php
@@ -9,6 +9,16 @@ if (session_status() == PHP_SESSION_NONE) {
 
     // Define the User class
     class User {
+        
+        // Add method to retrieve all users
+        public function get_all_users() {
+            $db = db_connect();
+            $stmt = $db->prepare("SELECT * FROM users");
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        
       
     }
 ?>
