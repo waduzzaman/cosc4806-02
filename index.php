e0424e92754218cc3f1ff5319bace88874fc2cbb
Md Mahbub E Waduzzaman <42499715-mwaduzzaman@users.noreply.replit.com>
2025-05-29 15:30:00 +0000

    login page added.

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <html>
   <head>
-    <title>PHP Test</title>
+    <title>Assignment 02</title>
   </head>
   <body>
+    <h1>Assignment 02</h1>
     <?php echo '<p>Hello World</p>'; ?> 
 
 </html>
---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+// login.php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<h1>Login Page</h1>
