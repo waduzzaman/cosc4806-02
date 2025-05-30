a3485d97ba9fc4d492b73776c8ac5104d43641ad
Md Mahbub  E Waduzzaman <45024227+waduzzaman@users.noreply.github.com>
2025-05-30 00:35:00 +0000

    add session and setup variables on registration page

--- register.php
+++ register.php
@@ -1,3 +1,9 @@
 <?php
+// Start session and include User class
+session_start();
+require_once('user.php');
 
+// Initialize variables
+$name = $username = $password = $confirm_password = "";
+$errors = [];
 ?>
