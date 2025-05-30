a988c204c344478b17511182405adf82e9a9020f
Md Mahbub  E Waduzzaman <45024227+waduzzaman@users.noreply.github.com>
2025-05-30 12:26:20 +0000

    final commit

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-// Database host address
+// host address
 define('DB_HOST',         'iix74.h.filess.io');
-// Database username
+// username
 define('DB_USER',         'cosc4806assignment2_possiblymy');
-// Database password 
+// password 
 define('DB_PASS',         $_ENV['DB_PASS']);
-// Database name to connect to
+// database name
 define('DB_DATABASE',     'cosc4806assignment2_possiblymy');
-// Database port number 
+// port number 
 define('DB_PORT',         '3305');
 
 ?>
